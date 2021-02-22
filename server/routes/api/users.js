4e7dfa17b0fb53eab484fb9ed02c21f2d1432647
Shehla Mushtaq <shehla.asim.78@gmail.com>
2021-02-23 02:16:48 +0500

    changes

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const User = require("../../models/users.js");
 const router = express.Router();
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcryptjs");
 //const users = require('../../Users');
-var jwt = require('jsonwebtoken')
-const dotenv = require('dotenv')
+var jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 dotenv.config();
 
 //===========================================Get all users
 router.get("/", async (req, res) => {
   try {
-
     const users = await User.find();
     console.log(users);
 
     res.json({
       status: 200,
       success: true,
-      data: users
-    })
+      data: users,
+    });
   } catch (e) {
     res.json({
       status: 404,
       success: false,
-      error: e.message
-    })
+      error: e.message,
+    });
     // res.status(404).json({ success: false, error: err.message });
   }
 });
@@ -35,158 +34,167 @@ router.post("/add", async (req, res) => {
   let { pwd, email, name } = req.body;
   var salt = bcrypt.genSaltSync(10);
   var hash = bcrypt.hashSync(pwd, salt);
-  console.log("hash=====", hash)
-  let newUser = { pwd: hash, email, name }
+  console.log("hash=====", hash);
+  let newUser = { pwd: hash, email, name };
   try {
     User.findOne({ email: req.body.email })
-      .then(user => {
+      .then((user) => {
         if (!user) {
           //// save
-          User.create(newUser)
-            .then(usr => res.json({
+          User.create(newUser).then((usr) =>
+            res.json({
               success: true,
               dbid: usr._id,
-              status: 201
-            }))
-        }//if
-
+              status: 201,
+            })
+          );
+        } //if
       })
-      .catch(err => res.json({
-        error: err,
-        status: 404
-      }))
-
-
-
+      .catch((err) =>
+        res.json({
+          error: err,
+          status: 404,
+        })
+      );
   } catch (err) {
     console.log(err);
     res.status(400).json({ success: false, error: err.message });
   }
-
 });
 
 //===================================================================Get single user
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     res.status(200).json({ success: true, data: user });
   } catch (err) {
     res.status(400).json({ success: false, error: err.message });
   }
-
-
 });
-router.post('update/:id', async (req, res) => {
-  console.log('update')
+router.post("update/:id", async (req, res) => {
+  console.log("update");
   const user = await User.findByIdAndUpdate(req.params.id, req.body);
   res.json({
     success: true,
     status: 200, //ok
     data: user,
-    msg: 'updated successfully'
-  })
-
-
-})
-router.delete('/:id', async (req, res) => {
+    msg: "updated successfully",
+  });
+});
+router.delete("/:id", async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
     res.json({
       success: true,
       status: 200, //ok
-      msg: 'post is deleted successfully'
-    })
-
+      msg: "post is deleted successfully",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-})
+});
 
 // let id = parseInt(req.params.id)
 // console.log(id)
 // let result = users.filter((item) => item.id == id)
 // res.json(result[0])
-router.get('/:id', async (req, res) => {
-
+router.get("/:id", async (req, res) => {
   res.json({
     success: true,
     status: 200, //ok
-    data: users
-  })
-
-})
-router.delete('/:id', async (req, res) => {
+    data: users,
+  });
+});
+router.delete("/:id", async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
-    console.log(req.params.id)
+    console.log(req.params.id);
     res.json({
       success: true,
       status: 200, //ok
-      msg: 'users is deleted successfully'
-    })
-
+      msg: "users is deleted successfully",
+    });
   } catch (error) {
-    console.log(".....", error)
+    console.log(".....", error);
   }
-
-})
+});
 // });
-router.post('/login', async (req, res) => {
-  const { pwd, email } = req.body;
-  console.log(req.body)
+router.post(
+  "/login",
+  async (req, res) => {
+    const { pwd, email } = req.body;
+    //console.log("body", req.body);
 
-  try {
-    User.findOne({ email })
-      .then(user => {
-        console.log(user)
-        bcrypt.compare(pwd, user.pwd)
-        .then((isMatch) => {
-          if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
-          else {
-            jwt.sign({id: user._id, email:user.email}, process.env.JWT_SECRET, function(err, token) {
-              let onLineUser = {id:user._id, name:user.name, email:user.email,token}
-              if(err) return res.json({status:400, msg:"no token generated"})
-              console.log(token);
-              res.json({
-              status: 200,
-              data: onLineUser,
-              msg: "login success",
-              token
-            })
- });
+    try {
+      User.findOne({ email }).then((user) => {
+        //console.log(user);
 
-          }//else
-        }) //bcypt then
+        if (!user) {
+          return res.status(200).json({ status: false, msg: "User Not Found" });
+        } else {
+          bcrypt.compare(pwd, user.pwd).then((isMatch) => {
+            if (!isMatch) {
+              return res
+                .status(200)
+                .json({ status: false, msg: "Invalid credentials" });
+            } else {
+              jwt.sign(
+                { id: user._id, email: user.email },
+                process.env.JWT_SECRET,
+                function (err, token) {
+                  let onLineUser = {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    token,
+                  };
+
+                  if (err) {
+                    return res.json({
+                      status: false,
+                      msg: "no token generated",
+                    });
+                  } else {
+                    console.log(token);
+                    res.json({
+                      status: true,
+                      data: onLineUser,
+                      msg: "login success",
+                      token,
+                    });
+                  }
+                }
+              );
+            } //else
+          }); //bcypt then
+        }
+
         // .catch(err => console.log('.....', err))
-      })//usr then
-  }//try
-  catch (error) {
-    console.log(error)
-  }
-}//post
-)
+      }); //usr then
+    } catch (error) {
+      //try
+      console.log(error);
+    }
+  } //post
+);
 
 ///=========logout
-router.delete('/logout',(req,res)=>{
-req.session.destroy((err)=>{
-if(err){
-  res.json({
-    status: 400,
-    msg: "logout failed"
-  })
-
-} else{
-
-  res.clearCookie("session-id");
-  res.json({
-    status: 200,
-    msg: "logout success"
-  })
-
-}
-})
-})
+router.delete("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.json({
+        status: 400,
+        msg: "logout failed",
+      });
+    } else {
+      res.clearCookie("session-id");
+      res.json({
+        status: 200,
+        msg: "logout success",
+      });
+    }
+  });
+});
 // router.post('/authcheck',(req,res)=>{
 //   console.log('authcheck')
 //   /// if user loggedIn
@@ -199,6 +207,4 @@ if(err){
 //   }
 // })
 
-
-
-module.exports = router
+module.exports = router;
